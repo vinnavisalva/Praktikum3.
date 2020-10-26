bfb92b5a5c1e1e87a38994a9173f4caa9736cd50
vinnavisalvamm <visalva57@gmail.com>
2020-10-26 18:53:43 +0700

    Initial commit

---
+++ app/src/main/java/com/vinnavisalvamm_18102215/lifecycleappp/HalamanDuaActivity.kt
@@ -0,0 +1,41 @@
+package com.vinnavisalvamm_18102215.lifecycleappp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class HalamanDuaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_dua)
+    }
+    fun printState(msg: String){
+        Log.d("ActivityState",msg)
+        Toast.makeText(applicationContext,msg, Toast.LENGTH_SHORT).show()
+    }
+    override fun onStart(){
+        super.onStart()
+        printState("Halaman satu On Start")
+    }
+    override fun onResume(){
+        super.onResume()
+        printState("Halaman satu On Resume")
+    }
+    override fun onPause(){
+        super.onPause()
+        printState("Halaman satu On Pause")
+    }
+    override fun onStop(){
+        super.onStop()
+        printState("Halaman satu On Stop")
+    }
+    override fun onRestart(){
+        super.onRestart()
+        printState("Halaman satu On Restart")
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        printState("Halaman satu On Destroy")
+    }
+}
